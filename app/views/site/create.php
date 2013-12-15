5ae667235aa5e6c7ce131e7cc0d6773e6c9d7a96
blueowner <blueowner@live.it>
2013-12-15 20:49:29 +0100

    Last one?

--- app/views/site/create.php
+++ app/views/site/create.php
@@ -5,5 +5,5 @@
 <form method="post" action="<?= base('create/store') ?>" class="form-create">
 	<input type="text" name="name" placeholder="John Doe">
 	<input type="tel" name="number" placeholder="338 312 6741" required>
-	<input type="submit" class="button main">
+	<input type="submit" value="Add" class="button main">
 </form>
